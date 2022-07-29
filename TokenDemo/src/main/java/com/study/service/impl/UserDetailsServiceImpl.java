549dbdda46b6b7b8d6de950e4883703f28e132d9
xgl <34029067@qq.com>
2022-07-29 11:39:59 +0800

    第一次提交：SpringSecurity环境搭建

---
+++ TokenDemo/src/main/java/com/study/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.study.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.study.domain.User;
+import com.study.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //根据用户名查询用户信息
+
+        //如果查询不到数据就通过抛出异常来给出提示
+
+        //TODO 根据用户查询权限信息 添加到LoginUser中
+        
+        //封装成UserDetails对象返回 
+        return null;
+    }
+}
---
+++ TokenDemo/src/test/java/com/study/MapperTest.java
@@ -0,0 +1,24 @@
+package com.study;
+
+import com.study.domain.User;
+
+import com.study.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testUserMapper(){
+        List<User> users = userMapper.selectList(null);
+        System.out.println(users);
+    }
+}
